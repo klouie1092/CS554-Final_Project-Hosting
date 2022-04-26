eb2feffbcaa42063a61b98594342e5319e0a47ec
Thebert98 <92902192+Thebert98@users.noreply.github.com>
2022-04-26 16:39:11 -0400

    adding contents of components
    foundation files with Auth implmented

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,1 +1,38 @@
+import React from 'react'
+import '../App.css';
+import Account from './Account';
+import {BrowserRouter as Router,Route, Routes} from 'react-router-dom'
+import Home from './Home';
+import Landing from './Landing'
+import Navigation from './Navigation';
+import SignIn from './SignIn'
+import SignUp from './SignUp'
 
+import {AuthProvider} from "../firebase/Auth"
+import PrivateRoute from './PrivateRoute';
+function App() {
+  return (
+    <AuthProvider>
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          <Navigation />
+        </header>
+      </div>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/home' element={<PrivateRoute />}>
+          <Route path='/home' element={<Home />} />
+        </Route>
+        <Route path='/account' element={<PrivateRoute />}>
+          <Route path='/account' element={<Account />} />
+        </Route>
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+      </Routes>
+    </Router>
+  </AuthProvider>
+  );
+}
+
+export default App;
