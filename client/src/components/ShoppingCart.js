5b36337634c7bcb04a8f29ca1fff9a74a587b81c
DanielWu1 <Tony153265964@gmail.com>
2022-05-02 20:28:24 -0400

    add more detail about shoppingcart page

--- client/src/components/ShoppingCart.js
+++ client/src/components/ShoppingCart.js
@@ -8,6 +8,8 @@ import '../App.css';
 function ShoppingCart() {
     const {currentUser} = useContext(AuthContext);
     const [ shopcart, setShopcartData ] = useState(undefined);
+    const [ error, setError ] = useState(false);
+    const [ loading, setLoading ] = useState(true);
     console.log(currentUser.email)
     useEffect(() =>{
         async function fetchData(){
@@ -15,22 +17,53 @@ function ShoppingCart() {
                 const data = await axios.get("http://localhost:4000/usershopcart/" + currentUser.email);
                 console.log(data.data)
                 //useContext(data.data.results);
+                setLoading(false)
+                setError(false)
                 setShopcartData(data.data);
-                //console.log(count)
+                console.log(shopcart===undefined)
 
             }
             catch(e){
+                setError(true)
                 console.log(e)
             }
         }
         fetchData();
 
     }, []);
-    return (
-        <div>
-            <h2>This is the Shopping Cart page</h2>
-        </div>
-    );
+    if (error === true){
+        return (
+            <div>
+                <h2>404 page not find</h2>
+            </div>
+        )
+    }
+    else if(loading === true){
+        return(
+            <div>
+                <h2>Loading . . .</h2>
+            </div>
+        )
+    }
+    else{
+        //console.log(shopcart)
+        if(shopcart === undefined || shopcart.length === 0){
+            console.log('aaaa')
+            return(
+                <div>
+                    <h2>Your didn't add any candy in to shopping cart go add some!!!</h2>
+                </div>
+            )
+        }
+        else{
+            return (
+                <div>
+                    <h2>This is the Shopping Cart page</h2>
+                </div>
+            );
+        }
+    }
+    
 }
 
 export default ShoppingCart;
