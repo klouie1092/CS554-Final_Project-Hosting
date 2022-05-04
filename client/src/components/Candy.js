d0331e7eba1c62ef750df559e65854fac74a6aca
DanielWu1 <Tony153265964@gmail.com>
2022-05-04 17:22:44 -0400

    shoppingcart work now

--- client/src/components/Candy.js
+++ client/src/components/Candy.js
@@ -1,49 +1,111 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect,useContext} from 'react';
 import axios from 'axios';
 import {useParams } from "react-router-dom";
-
+import { AuthContext } from '../firebase/Auth';
 
 const Candy = () =>{
-    const [candyInfo, setCandyInfo] = useState(undefined)
-    const params = useParams();
+  const {currentUser} = useContext(AuthContext);
+  const [candyInfo, setCandyInfo] = useState(undefined)
+  const [candyHave, setCandyHave] = useState(0)
+  const params = useParams();
+  const intn = /^\+?[1-9][0-9]*$/
+  let but 
 
 
-    useEffect(() => {
-        async function fetchData() {
-          try {
-            let candyId = params.id
-            const {data} = await axios.get('http://localhost:4000/Candy/' + candyId);
-            setCandyInfo(data);
-          } catch (e) {
-            console.log(e);
+  useEffect(() => {
+      async function fetchData() {
+        try {
+          let candyId = params.id
+          const {data} = await axios.get('http://localhost:4000/Candy/' + candyId);
+          const have = await axios.get('http://localhost:4000/usershopcart/' + currentUser.email)
+          setCandyInfo(data);
+          let changeData = await have.data.filter((e) => {
+            if (e.id === candyId){
+              return [0]
+            }
+          })
+          //console.log(changeData[0].numbers)
+          if(changeData[0].numbers){
+            //console.log('aaa')
+            setCandyHave(changeData[0].numbers)
+            //console.log(candyHave)
           }
+          console.log(candyHave)
+        } catch (e) {
+          console.log(e);
         }
-        fetchData();
-      }, []);
-
-    if(candyInfo === undefined){
-      return(
-        <div>
-          <h1>Sorry, there is no such candy</h1>
-        </div>
-      )
+      }
+      fetchData();
+  }, []);
+  const changeCandy = async () => {
+    let numberha = document.getElementById('number').value
+    let numberha1 = Number(numberha)
+    if (isNaN(numberha1) || isNaN(numberha1) || isNaN(numberha1)){
+      alert('input must be number')
+      document.getElementById('number').value = ''
     }
+    else if(intn.test(numberha) === false){
+      alert('input must be integer')
+      document.getElementById('number').value = ''
+    }
+    else if (numberha1 > candyInfo.stock){
+      alert('input must less than candy left')
+      document.getElementById('number').value = ''
+    }
+    else{
+      let total = numberha1 + candyHave
+      const body = {id: params.id, name : candyInfo.name, price: candyInfo.price, image:candyInfo.image, numbers:total}
+      try{
+        const setdata = await axios.put('http://localhost:4000/usershopcart/'+ currentUser.email, body,).then(res=>{console.log(res)})
+        alert('you add it to shopping cart')
+      }
+      catch(e){
+        alert(e)
+      }
+      
+      document.getElementById('number').value = ''
+    }
+    
+  };
 
+
+  if(candyInfo === undefined){
     return(
-      <div className='Candy-body'>
-        <img src = {candyInfo.image} width= {400} height = {400} alt = "Candy image" />
-        <h1>{candyInfo&&candyInfo.name}</h1>
-        <br />
-        <br />
-        <h2>Price: ${candyInfo&&candyInfo.price}</h2>
-        <br />
-        <h3>There is {candyInfo&&candyInfo.stock} left</h3>
-        <br />
-        <h4>{candyInfo&&candyInfo.manufacturer}</h4>
-        <h5>{candyInfo&&candyInfo.descrption}</h5>
-
+      <div>
+        <h1>Sorry, there is no such candy</h1>
       </div>
     )
+  }
+
+  return(
+    <div className='Candy-body'>
+      <img src = {candyInfo.image} width= {400} height = {400} alt = "Candy image" />
+      <h1>{candyInfo&&candyInfo.name}</h1>
+      <br />
+      <br />
+      <h2>Price: ${candyInfo&&candyInfo.price}</h2>
+      <br />
+      <h3>There is {candyInfo&&candyInfo.stock} left</h3>
+      <br />
+      <h4>{candyInfo&&candyInfo.manufacturer}</h4>
+      <h5>{candyInfo&&candyInfo.descrption}</h5>
+      <div className='add'>
+      {currentUser&&(<div className='input-selection'>
+        <label>
+          you have {candyHave} Candies:
+          <input
+            id='number'
+            name='number'
+            placeholder='number that you want to add'
+          />
+        </label>
+      </div>)}
+      {currentUser&&(<button onClick={changeCandy}> add to card</button>)}
+      {!currentUser&&(<h6> login for add candy to shopping cart</h6>)}
+    </div>
+
+    </div>
+  )
 };
 
 export default Candy
--- server/App.js
+++ server/App.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
 const configRoutes = require('./routes');
-cors = require("cors")
+//cors = require("cors")
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
