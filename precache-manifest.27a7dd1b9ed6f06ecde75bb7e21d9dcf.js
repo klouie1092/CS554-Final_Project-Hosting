43d59365b48e78b204887eb94cc7da058a4e6df3
klouie1092 <klouie1092@gmail.com>
2022-05-12 03:04:16 -0400

    Updates

---
+++ precache-manifest.27a7dd1b9ed6f06ecde75bb7e21d9dcf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd97408b673539d64daf720ba35cf3e",
+    "url": "/CS554-Final_Project/index.html"
+  },
+  {
+    "revision": "d4402b0d72dec5fdb22c",
+    "url": "/CS554-Final_Project/static/css/main.ee745a06.chunk.css"
+  },
+  {
+    "revision": "0337ede9c79aea432565",
+    "url": "/CS554-Final_Project/static/js/2.ddfde038.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/CS554-Final_Project/static/js/2.ddfde038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a562403a05b6068fdf71",
+    "url": "/CS554-Final_Project/static/js/3.a6470091.chunk.js"
+  },
+  {
+    "revision": "cbe43123dd724b1287aa275afd38c90e",
+    "url": "/CS554-Final_Project/static/js/3.a6470091.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "488e3f688968f52dcf67",
+    "url": "/CS554-Final_Project/static/js/4.4a9aee35.chunk.js"
+  },
+  {
+    "revision": "9127c945005b1c05fcb5ed1efd0e0e53",
+    "url": "/CS554-Final_Project/static/js/4.4a9aee35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27afeb56120579af2d3f",
+    "url": "/CS554-Final_Project/static/js/5.18b1f747.chunk.js"
+  },
+  {
+    "revision": "4c2f580656954a1ad1c84a3bcb195d1c",
+    "url": "/CS554-Final_Project/static/js/5.18b1f747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4402b0d72dec5fdb22c",
+    "url": "/CS554-Final_Project/static/js/main.5db1fa4e.chunk.js"
+  },
+  {
+    "revision": "09612bbacdd294e3a523",
+    "url": "/CS554-Final_Project/static/js/runtime-main.69d9797f.js"
+  }
+]);
